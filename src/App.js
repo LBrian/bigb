f93e16b10169367fbe6c50f9bc0e67a25fd02550
Brian Liu <ralphbliu@gmail.com>
2020-12-10 18:28:24 +1100

    Tweaked favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="avatar.png" className="App-logo" alt="logo" />
-        Hi, my name is Brian, Im a frontend/React/JS developer, more to come
+    <div>
+      <header>
+        <img src="avatar.png" alt="logo" />
+        I'm a frontend/React/JS developer, more to come...
       </header>
     </div>
   );
