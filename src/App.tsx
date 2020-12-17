a505b467a3fb9787f70cf70ca1bc596ea60f97fb
Brian Liu <ralphbliu@gmail.com>
2020-12-17 17:56:58 +1100

    Set up TS/Linting and Prettier

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Header } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div>
+        <img src="avatar.png" alt="logo" />
+        <span>Im a frontend/React/JS developer, more to come...</span>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface HeaderProps {}
+
+const Header = (prop: HeaderProps) => {
+  return <header className="bg-black h-20 text-white"></header>;
+};
+
+export default Header;
