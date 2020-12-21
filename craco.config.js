28487a258915d680124cbf59bbd00b70477e4a4c
Brian Liu <ralphbliu@gmail.com>
2020-12-22 10:58:22 +1100

    Changed react to preact

--- craco.config.js
+++ craco.config.js
@@ -7,4 +7,20 @@ module.exports = {
       ],
     },
   },
+  webpack: {
+    "alias": {
+      "react": "preact/compat",
+      "react-dom": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+    }
+  },
+  babel: {
+    plugins: [
+      ["@babel/plugin-transform-react-jsx", {
+        "runtime": "automatic",
+        "importSource": "preact",
+        "throwIfNamespace": false
+      }]
+    ]
+  }
 }
