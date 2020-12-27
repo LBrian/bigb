f0ce02064b2d12ec1a5566ffe715af13e8645a58
Brian Liu <ralphbliu@gmail.com>
2020-12-28 09:32:08 +1100

    Removed duplicate folder

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,8 +9,8 @@ const Header = (prop: HeaderProps) => {
   const { darkMode, toggleDarkMode } = useContext(AppContext);
 
   return (
-    <header className="h-16 sticky shadow-lg sm:px-6 flex items-center font-sans">
-      <div className="flex-1 text-xl font-bold whitespace-nowrap text-center sm:text-left">
+    <header className="h-16 sticky shadow-lg px-6 flex items-center font-sans">
+      <div className="flex-1 text-lg sm:text-xl font-bold whitespace-nowrap text-left">
         Brian Yueh-Peng Liu
       </div>
       <wired-toggle checked={darkMode} onchange={toggleDarkMode} />
