374632707b57f2703877babee84945d890059bd1
Brian Liu <ralphbliu@gmail.com>
2021-01-03 15:33:54 +1100

    Removed unused import bundle impact performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'wired-elements';
 import { useEffect, useState } from 'preact/hooks';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
