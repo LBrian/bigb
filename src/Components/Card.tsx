af7ceac10bef9bd8fcd0520622a4ef54e20c5e0f
Brian Liu <ralphbliu@gmail.com>
2021-01-13 22:43:10 +1100

    Tweaked styles

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,5 +1,7 @@
-import React, { HTMLAttributes, PropsWithChildren } from 'react';
+import React, { useContext, HTMLAttributes, PropsWithChildren } from 'react';
 import { useInView } from 'react-intersection-observer';
+
+import { AppContext } from 'App';
 
 interface CardsProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -20,23 +22,27 @@ const Card = ({
   children,
   className,
   threshold = 0.6,
-  bgColor = 'bg-gray',
+  bgColor,
   ...props
 }: PropsWithChildren<CardsProps>) => {
+  const { darkMode } = useContext(AppContext);
   const [ref, inView] = useInView({ threshold, delay: 200 });
 
   return (
     <div className={`flip-card ${className ?? ''}`} {...props}>
       <div
         ref={ref}
-        className={`flip-card-content ${bgColor} ${
-          inView ? 'flip-card-in-view' : ''
+        className={`flip-card-content ${bgColor ? bgColor : ''} ${
+          inView ? 'flip-card-in-view' : 'opacity-40'
         }`}>
         <div className='flip-card-image'>
           <img src={imageSrc} alt={title} loading='lazy' />
         </div>
-        <div className='flip-card-info'>
-          <div className='flip-card-title'>{title}</div>
+        <div
+          className={`flip-card-info${
+            !darkMode && !bgColor ? ' text-blue-gray' : ''
+          }`}>
+          <div className='flip-card-title filter-drop-shadow'>{title}</div>
           <div className='flip-card-description'>{children}</div>
         </div>
       </div>
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,6 +1,5 @@
 export * from './Icons';
 export { default as Card } from './Card';
 export { default as Section } from './Section';
-export { default as CompanyPreview } from './CompanyPreview';
 export { DraculaName, DraculaString, DraculaNumber } from './Dracula';
 export { default as ShieldsBadge, ShieldsBadges } from './ShieldsBadge';
